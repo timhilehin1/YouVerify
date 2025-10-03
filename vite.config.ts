c7582792d91a2403214f308f0d824b3b0131a3fe
timilehin_princeps <timilehin.oladapo@princepsfinance.com>
2025-10-03 01:16:25 +0100

    added test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0", // or host: true
